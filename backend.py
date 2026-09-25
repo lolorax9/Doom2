0284505ff6ff4b5173b064746468f29a60c1cb10
laurentvachon <laurent-vachon@hotmail.com>
2026-09-25 12:00:25 -0400

    Ajout d'un ConnectionManager

--- backend.py
+++ backend.py
@@ -2,94 +2,113 @@ from __future__ import annotations
 import asyncio
 from websockets.asyncio.server import serve, ServerConnection
 import json
-
-
-connected_clients: set[Client] = set()
-alltime_clients: set[Client] = set()
-def toJSON(content : str, type : str, author : str, counter : int) -> dict[str, str | int]:
-    return {"content": content, "type": type, "author": author, "counter": counter}
 log : list[Package] = []
+class ConnectionManager():
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+
+        return cls._instance
+
+    def __init__(self):
+        if not self._initialized: 
+            self.connected_clients = set()
+            self.clients = set()
+            self._initialized = True
+    async def disconnect(self, client):
+        client.disconnect()
+        del client.connection
+        self.connected_clients.remove(client)
+        await asyncio.gather(*(client.send(Package(f"{client.name} vient de se déconnecter", "serveur", "connection")) for other in self.connected_clients))
+    async def identify(self, identification, connection):
+        error = ""
+        infos = identification.split("\n")
+        name = infos[0].strip()
+        password = infos[1].strip()
+        fclient = None
+        for client in self.clients:
+            if client.name == name:
+                if client in self.connected_clients:
+                    await connection.send(Package("double", "serveur", "identification"))
+                    return 
+                elif client.password != password:
+                    await connection.send(Package("password", "serveur", "identification"))
+                    return
+                else:
+                    fclient = client
+        if not fclient:
+            fclient = Client(name, password)
+            self.clients.add(fclient)
+        fclient.connect(connection)
+        self.connected_clients.add(fclient)
+        await fclient.send(Package("ok", "serveur", "identification"))
+        for other in self.connected_clients:
+            if other == fclient:
+                    continue
+            await fclient.send(Package(f"{other.name} est connecté", "serveur", "connection"))
+        print(f"{fclient.name} vient de se connecter")
+        await asyncio.gather(*(fclient.send(package) for package in log))
+        await asyncio.gather(*(client.send(Package(f"{fclient.name} vient de se connecter", "serveur", "connection")) for client in self.connected_clients))
+        return fclient
+
 class Package():
-    def __init__(self, content : str = "", author : str = "", type : str = "", counter : int = 0):
+    def __init__(self, content : str = "", author : str = "", type : str = ""):
         self.content = content
         self.author = author
         self.type = type
-        self.counter = counter
-        
-
-    async def send(self, websocket : ServerConnection) -> None:
-        await websocket.send(json.dumps(toJSON(self.content, self.type, self.author, self.counter)))
-
+    def toJSON(self):
+        return {"content": self.content, "author": self.author, "type": self.type}
     @staticmethod
     def receive(message : str | bytes):
         data = json.loads(message)
-        return Package(data["content"], data["author"], data["type"], data["counter"])
+        return Package(data["content"], data["author"], data["type"])
 
 class Client():
-    def __init__(self, websocket : ServerConnection):
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        self.connection = None
+    async def send(self, package):
+        if self.connection:
+            await self.connection.send(package)
+    def connect(self, connection):
+        self.connection = connection
+    def disconnect(self):
+        self.connection = None
+        
+
+        
+
+class Connection():
+    def __init__(self, websocket):
         self.websocket = websocket
-        self.name = ""
-        self.id = str(websocket.remote_address[0]) + str(websocket.remote_address[1])
-        connected_clients.add(self)
-        self.counter = 0
-        self.identified = False
-
-    def delete(self):
-        self.identified = False
-        connected_clients.remove(self)
-
-    async def identify(self, identification : str):
-        infos = identification.split("\n")
-        self.name = infos[0].strip()
-        self.password = infos[1].strip()
-        print(self.name)
-        print(self.password)
-        for client in alltime_clients:
-            if client.name == self.name:
-                if client.password != self.password:
-                    await self.sendPackage(Package("password", "serveur", "identification"))
-                    return 
-        alltime_clients.add(self)
-        self.identified = True
-        print("New user")
-        await self.sendPackage(Package("ok", "serveur", "identification"))
-        for other in connected_clients:
-            if other == self:
-                continue
-            await self.sendPackage(Package(f"{other.name} est connecté", "serveur", "connection"))
-        print(f"{self.id} vient de se connecter")
-        await asyncio.gather(*(self.sendPackage(package) for package in log))
-        await asyncio.gather(*(client.sendPackage(Package(f"{self.name} vient de se connecter", "serveur", "connection", )) for client in connected_clients))
-
-                
-
-    async def sendPackage(self, package : Package) -> None:
-        if package.type == "message" or package.type == "connection":
-            self.counter += 1 
-            package.counter += 1 
-        await package.send(self.websocket)
+    async def send(self, package):
+        await self.websocket.send(json.dumps(package.toJSON()))
     
 async def echo_handler(websocket : ServerConnection):
-    client = Client(websocket)
-    print(f"Connection opened: {client.id}")
-    await client.sendPackage(Package("PING", "serveur", "ping"))
-    print(f"{client.id} <- PING")
+    manager = ConnectionManager()
+    connection = Connection(websocket)
+    client = None
+    print(f"Connection opened: {connection.websocket}")
+    await connection.send(Package("PING", "serveur", "ping"))
+    print(f"{connection.websocket} <- PING")
     try:
-        async for package in client.websocket:
+        async for package in connection.websocket:
             package = Package.receive(package)
-            print(f"{client.id} -> {package.content}")
+            print(f"{connection.websocket} -> {package.content}")
             if package.type == "identification":
-                await client.identify(package.content)
-            if package.type == "message":
+                client = await manager.identify(package.content, connection)
+            elif package.type == "message":
                 print("New message")
                 log.append(package)
-                await asyncio.gather(*(client.sendPackage(package) for client in connected_clients))
+                await asyncio.gather(*(client.send(package) for client in manager.connected_clients))
     finally:
-        print(f"Connection closed: {client.id}")
-        name = client.name
-        client.delete()
-        await asyncio.gather(*(client.sendPackage(Package(f"{name} vient de se déconnecter", "serveur", "connection")) for client in connected_clients))
-        # timer_task.cancel()
+        print(f"Connection closed: {connection.websocket}")
+        await manager.disconnect(client)
+        
 
 async def main():
     async with serve(echo_handler, "localhost", 8765):
